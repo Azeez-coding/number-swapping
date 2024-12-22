93c1b84f1bc0ac9147f27c272032c8490b872a8b
Azeez-coding <meetmusaonline03@gmail.com>
2024-12-22 16:38:58 +0100

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,21 @@
+num1 = 45
+num2 = 3
+
+print("Number 1",num1)
+print("Number 2",num2)
+print("addition :",num1+num2)
+print("subtraction :",num1-num2)
+print("Division :",num1/num2)
+print("product :",num1*num2)
+print("Floor Division :",num1//num2)
+print("Modulus :",num1%num2)
+print("square :",num2**num2)
+print("square root:",num1**0.5)
+
+print("equal ? :",num1==num2)
+print("Number 1 greater :",num1>num2)
+print("Number 2 greater :",num2>num1)
+print("Not equal ?:",num1!=num2)
+
+result = num1/2 + num2**2+10
+print("Result of given equation is :",result)
---
+++ activity3.py
@@ -0,0 +1,7 @@
+first_name = "codingal"
+last_name = "education"
+full_name =first_name+last_name
+example = "haa"*5
+
+print("first name:", first_name)
+print("last name:", last_name)
